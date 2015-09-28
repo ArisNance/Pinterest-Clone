47b651cc071b5cc99a469fa08b191dc5df0ea275
Aris Nance <nance.aris@gmail.com>
2015-09-28 06:46:26 +0000

    first content push

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  resources :pins
+  devise_for :users
   root 'home#index'
+  get 'home/about'
+  
+  
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -55,4 +61,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
